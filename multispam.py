8363b5de0f0bf62ed0e14124dae407649e933de3
Thomas Dencker <thomas.dencker@stud.uni-goettingen.de>
2018-04-16 16:27:53 +0200

    improved options

--- multispam.py
+++ multispam.py
@@ -16,21 +16,19 @@ def check_positive(value):
 		raise argparse.ArgumentTypeError("%s is an invalid positive int value" % value)
 	return ivalue
 
-
 def build_opts():
 	import argparse
 	parser = argparse.ArgumentParser(description = '''Build phylogenetic trees from DNA sequence data with Maximum-Likelihood
 		 and a quartet based super tree approach''', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-	parser.add_argument("-i", nargs="?", type=str, help="Input file (multi-fasta file)", required=True)
-	parser.add_argument("-o", nargs="?", type=str, help="Output file (tree file in newick-format)", default="outtree")
-	parser.add_argument("-k", metavar="w", nargs="?", type=int, choices=range(6,17), help="weight of the pattern \
-		(the number of matching positions)", default=10)
-	parser.add_argument("-d", nargs="?", type=check_positive, help="number of don't care positions in the pattern", default=100)
-	parser.add_argument("-n", nargs="?", type=check_positive, help="number of sampled quartet blocks", default=1000000)
-	parser.add_argument("-t", nargs="?", type=check_positive, help="number of threads", default=1)
-	parser.add_argument("-m", metavar="--mem-save", nargs="?", type=bool, \
-		const=True, default=False, help="memory saving mode")
-	parser.add_argument('--version', action='version', version='Multi-SpaM 1.0')
+	parser.add_argument("-i", metavar="<string>", type=str, help="[Required] Input file (multi-fasta file)", required=True)
+	parser.add_argument("-o", metavar="<string>", type=str, help="Output file (tree file in newick-format)", default="outtree")
+	parser.add_argument("-k", "-w", metavar="<int>", type=int, choices=range(6,17), help="weight of the pattern \
+	 	(the number of matching positions)", default=10, dest="k")
+	parser.add_argument("-d", metavar="<int>", type=check_positive, help="number of don't care positions in the pattern", default=100)
+	parser.add_argument("-n", metavar="<int>", type=check_positive, help="number of sampled quartet blocks", default=1000000)
+	parser.add_argument("-t", metavar="<int>", type=check_positive, help="number of threads", default=1)
+	parser.add_argument("-m", "--mem-save", help="memory saving mode", dest="m", action="store_true")
+	parser.add_argument("-v", "--version", action="version", version='Multi-SpaM 1.0')
 
 	return parser.parse_args()
 

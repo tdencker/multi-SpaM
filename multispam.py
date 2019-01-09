2490af7f529da9de48e1d19242abdf2eeaccd506
Thomas Dencker <thomas.dencker@stud.uni-goettingen.de>
2019-01-09 13:53:52 +0100

    string consistency

--- multispam.py
+++ multispam.py
@@ -13,42 +13,42 @@ def check_positive(value):
 	import argparse
 	ivalue = int(value)
 	if ivalue <= 0:
-		raise argparse.ArgumentTypeError("%s is an invalid positive int value" % value)
+		raise argparse.ArgumentTypeError('%s is an invalid positive int value' % value)
 	return ivalue
 
 def build_opts():
 	import argparse
 	parser = argparse.ArgumentParser(description = '''Build phylogenetic trees from DNA sequence data with Maximum-Likelihood
 		 and a quartet based super tree approach''', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-	parser.add_argument("-i", metavar="<string>", type=str, help="[Required] Input file (multi-fasta file)", required=True)
-	parser.add_argument("-o", metavar="<string>", type=str, help="Output file (tree file in newick-format)", default="outtree")
-	parser.add_argument("-k", "-w", metavar="<int>", type=int, choices=range(6,17), help="weight of the pattern \
-	 	(the number of matching positions)", default=10, dest="w")
-	parser.add_argument("-d", metavar="<int>", type=check_positive, help="number of don't care positions in the pattern", default=100)
-	parser.add_argument("-n", metavar="<int>", type=check_positive, help="number of sampled quartet blocks", default=1000000)
-	parser.add_argument("-t", metavar="<int>", type=check_positive, help="number of threads", default=1)
-	parser.add_argument("--mem-save", help="memory saving mode", dest="m", action="store_true")
-	parser.add_argument("--show-stats", help="additional stats (mostly for debugging)", dest="s", action="store_true")
-	parser.add_argument("--bootstrap", help="do bootstrapping", dest="b", action="store_true")
-	parser.add_argument("-v", "--version", action="version", version='Multi-SpaM 1.0')
+	parser.add_argument('-i', metavar='<string>', type=str, help='[Required] Input file (multi-fasta file)', required=True)
+	parser.add_argument('-o', metavar='<string>', type=str, help='Output file (tree file in newick-format)', default='outtree')
+	parser.add_argument('-k', '-w', metavar='<int>', type=int, choices=range(6,17), help='weight of the pattern \
+	 	(the number of matching positions)', default=10, dest='w')
+	parser.add_argument('-d', metavar='<int>', type=check_positive, help='number of don\'t care positions in the pattern', default=100)
+	parser.add_argument('-n', metavar='<int>', type=check_positive, help='number of sampled quartet blocks', default=1000000)
+	parser.add_argument('-t', metavar='<int>', type=check_positive, help='number of threads', default=1)
+	parser.add_argument('--mem-save', help='memory saving mode', dest='m', action='store_true')
+	parser.add_argument('--show-stats', help='additional stats (mostly for debugging)', dest='s', action='store_true')
+	parser.add_argument('--bootstrap', help='do bootstrapping', dest='b', action='store_true')
+	parser.add_argument('-v', '--version', action='version', version='Multi-SpaM 1.0')
 
 	return parser.parse_args()
 
 def build_arg_list(opts, base_dir, tmp_file):
 	import os
-	argl = [os.path.join(base_dir, "bin/multi-SpaM")]
+	argl = [os.path.join(base_dir, 'bin/multi-SpaM')]
 	if opts.m == True:
-		argl.append("--mem-save")
+		argl.append('--mem-save')
 	del opts.m
 	if opts.s == True:
-		argl.append("--show-stats")
+		argl.append('--show-stats')
 	del opts.s
 	for opt in vars(opts):
 		if opt == 'b':
 			continue
-		argl.append("-" + opt)
+		argl.append('-' + opt)
 		argl.append(str(getattr(opts,opt)))
-	argl[next(idx for idx, arg in enumerate(argl) if arg == "-o") + 1] = str(tmp_file)
+	argl[next(idx for idx, arg in enumerate(argl) if arg == '-o') + 1] = str(tmp_file)
 	return argl
 
 def build_id_map(filename):
@@ -56,7 +56,7 @@ def build_id_map(filename):
 	last_id = 0
 	with open(opts.i) as fasta:
 		for line in fasta:
-			if line.startswith(">"):
+			if line.startswith('>'):
 				id_map[last_id] = line[1:-1]
 				last_id += 1
 	return id_map
@@ -80,13 +80,13 @@ def simple_bootstrap(quartet_file, opts):
 	result_trees = []
 	for i in range(100):
 		print('\rSample {:>3}/100'.format(i), end = '')
-		id_tree_file = os.path.join(tmp_dir, id + str(i) + ".tree")
-		tmp_quartet_file = quartet_file + "_tmp"
+		id_tree_file = os.path.join(tmp_dir, id + str(i) + '.tree')
+		tmp_quartet_file = quartet_file + '_tmp'
 		sample = [random.choice(quartets) for _ in range(len(quartets))]
 		with open(tmp_quartet_file, 'w+') as out_file:
 			for quartet in sample:
 				out_file.write(quartet)
-		sp.check_call([os.path.join(base_dir, "bin/max-cut-tree"), "qrtt=" + tmp_quartet_file, "weights=off", "otre=" + id_tree_file], stdout = devnull)
+		sp.check_call([os.path.join(base_dir, 'bin/max-cut-tree'), 'qrtt=' + tmp_quartet_file, 'weights=off', 'otre=' + id_tree_file], stdout = devnull)
 		result_trees.append(fix_tree(id_tree_file, id_map))
 		os.remove(tmp_quartet_file)
 	print('\rSample 100/100')
@@ -104,7 +104,7 @@ def simple_bootstrap(quartet_file, opts):
 	os.rename('outtree', opts.o)
 	os.remove('outfile')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	import sys
 	import subprocess as sp
 	import uuid
@@ -114,7 +114,7 @@ if __name__ == "__main__":
 
 	id = str(uuid.uuid4())
 	if not sys.platform.startswith('linux') or not (sys.maxsize > 2 ** 32):
-		raise Exception("Multi-SpaM currently works only on 64-bit linux!")
+		raise Exception('Multi-SpaM currently works only on 64-bit linux!')
 	opts = build_opts()
 
 	base_dir = os.path.dirname(__file__)
@@ -123,12 +123,12 @@ if __name__ == "__main__":
 		os.makedirs(tmp_dir)
 
 	try:
-		open(os.path.join(base_dir, "bin/multi-SpaM")).close()
+		open(os.path.join(base_dir, 'bin/multi-SpaM')).close()
 	except FileNotFoundError:
-		raise Exception("Multi-SpaM was not installed yet. Run \"make\", then run the script again.")
+		raise Exception('Multi-SpaM was not installed yet. Run \'make\', then run the script again.')
 	
 	id_map = build_id_map(opts.i)
-	quartet_file = os.path.join(tmp_dir, id + ".quartets")
+	quartet_file = os.path.join(tmp_dir, id + '.quartets')
 	argl = build_arg_list(opts, base_dir, quartet_file)
 	devnull = open(os.devnull, 'w')
 
@@ -137,8 +137,8 @@ if __name__ == "__main__":
 	if opts.b == True:
 		simple_bootstrap(quartet_file, opts)
 	else:
-		id_tree_file = os.path.join(tmp_dir, id + ".tree")
-		sp.check_call([os.path.join(base_dir, "bin/max-cut-tree"), "qrtt=" + quartet_file, "weights=off", "otre=" + id_tree_file], stdout = devnull)
+		id_tree_file = os.path.join(tmp_dir, id + '.tree')
+		sp.check_call([os.path.join(base_dir, 'bin/max-cut-tree'), 'qrtt=' + quartet_file, 'weights=off', 'otre=' + id_tree_file], stdout = devnull)
 		result_tree = fix_tree(id_tree_file, id_map)
 		with open(opts.o, 'w+') as out_file:
 			out_file.write(result_tree + '\n')
